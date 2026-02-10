27c1b19383302e744b52b5ec626000661df88cd4
RUFUS NICHOLAS EBENEZER <5023148@it.fcrit.ac.in>
2026-02-10 15:05:38 +0530

    we good

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,12 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+	System.out.println("A = 5 ");
+	System.out.println("B = 3");
+        System.out.println("Sum = " + sum);
     }
 }
